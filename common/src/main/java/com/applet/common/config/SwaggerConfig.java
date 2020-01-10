b2798c4adcb01244986733d6ed6a82513d85b49e
谭良忠 <tanliangzhong@beadwallet.com>
2020-01-10 14:59:37 +0800

    swagger config

--- common/src/main/java/com/applet/common/config/SwaggerConfig.java
+++ common/src/main/java/com/applet/common/config/SwaggerConfig.java
@@ -4,12 +4,18 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Swagger 配置
@@ -22,7 +28,14 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
+        List<Parameter> params = new ArrayList<>();
+        ParameterBuilder wxCode = new ParameterBuilder().name("wxCode").modelRef(new ModelRef("string")).parameterType("header").required(false);
+        ParameterBuilder appletCode = new ParameterBuilder().name("appletCode").modelRef(new ModelRef("string")).parameterType("header").required(false);
+        params.add(wxCode.build());
+        params.add(appletCode.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(params)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
@@ -33,7 +46,7 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Swagger API")
-                .contact(new Contact("LiangZhongTan", "", "liangzhong.tan@outlook.com"))
+                .contact(new Contact("LiangZhong.Tan", "", "liangzhong.tan@outlook.com"))
                 .version("0.0.1")
                 .build();
     }
